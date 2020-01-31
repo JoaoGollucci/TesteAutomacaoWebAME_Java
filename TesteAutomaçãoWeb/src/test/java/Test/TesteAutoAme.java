31eb3fc580a7c568634dbe832c7469e467df4c58
João Gollucci <golluccitreck@gmail.com>
2020-01-31 18:27:38 -0300

    Update Criar Conta

--- TesteAutomaçãoWeb/src/test/java/Test/TesteAutoAme.java
+++ TesteAutomaçãoWeb/src/test/java/Test/TesteAutoAme.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -40,8 +41,39 @@ public class TesteAutoAme {
         navegador.findElement(By.xpath("//input[@id='email_create']")).sendKeys("teste"+Integer.toString(id)+"@ame.com.br");
         navegador.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
 
-        
+        navegador.findElement(By.xpath("//input[@id='id_gender1']")).click();
+        navegador.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("Teste");
+        navegador.findElement(By.xpath("//input[@id='customer_lastname']")).sendKeys("Teste");
+        navegador.findElement(By.xpath("//input[@id='passwd']")).sendKeys("teste123");
 
+        Select dia = new Select(navegador.findElement(By.xpath("//select[@id='days']")));
+        dia.selectByValue("11");
+        Select mes = new Select(navegador.findElement(By.xpath("//select[@id='months']")));
+        mes.selectByValue("6");
+        Select ano = new Select(navegador.findElement(By.xpath("//select[@id='years']")));
+        ano.selectByValue("1995");
+
+        navegador.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Teste");
+        navegador.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Teste");
+        navegador.findElement(By.xpath("//input[@id='address1']")).sendKeys("Rua Teste, 42");
+        navegador.findElement(By.xpath("//input[@id='city']")).sendKeys("Cidade Teste");
+
+        Select estado = new Select(navegador.findElement(By.xpath("//select[@id='id_state']")));
+        estado.selectByValue("1");
+
+        navegador.findElement(By.xpath("//input[@id='postcode']")).sendKeys("12345");
+
+        Select pais = new Select(navegador.findElement(By.xpath("//select[@id='id_country']")));
+        pais.selectByValue("21");
+
+        navegador.findElement(By.xpath("//input[@id='phone_mobile']")).sendKeys("999999999");
+        navegador.findElement(By.xpath("//input[@id='alias']")).sendKeys("Teste");
+
+        navegador.findElement(By.xpath("//button[@id='submitAccount']")).click();
+
+        String validar = navegador.findElement(By.xpath("//h1")).getText();
+        System.out.println(validar);
+        assertEquals("MY ACCOUNT", validar);
 
     }
 
